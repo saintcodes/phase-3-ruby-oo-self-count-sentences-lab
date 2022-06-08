be451bed00ff5e3b9b9e70540ae82a64e2999ebf
Michelle <clairsin@gmail.com>
2022-06-08 10:13:30 -0400

    commit tree 50b06957ea0bb244b8dd8caf4d10a91c64aa1557

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    end_with?(".") ? true : false
   end
 
   def question?
-
+    end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    squeeze('.!?').count('.!?')
+    # scan(/[^\.!?]/).map(&:strip).count
   end
 end
